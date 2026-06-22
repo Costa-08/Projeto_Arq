bb3449278c958863c41eb3cd6fdd963ae9944608
athospozo <athospozoo@gmail.com>
2026-06-22 15:07:49 -0300

    testando o empacotamento

---
+++ src/main/java/simulador/projecao/Launcher.java
@@ -0,0 +1,8 @@
+package simulador.projecao;
+
+public class Launcher {
+    public static void main(String[] args) {
+        // Apenas repassa a execução para a sua classe principal original
+        Tela.main(args);
+    }
+}
